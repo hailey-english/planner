3ad30e3cb5212cc6c9f8ca61c848edc4fb3f6e6e
hailey-english <elli22tiger@gmail.com>
2026-08-26 10:28:10 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
 
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Planner from "./Planner.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Planner />
+  </React.StrictMode>
+);
